f208eb03face740f68e7f1094f225cb78d40996e
unknown <patrickkeita80@yahoo.com>
2023-12-13 02:17:56 -0800

    Add migration

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
-    has_many :books
+  has_many :books
 
-    validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
