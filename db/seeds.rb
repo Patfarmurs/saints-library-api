ddebb7ac32906edd32f0b038299b2d359bf7152f
unknown <patrickkeita80@yahoo.com>
2023-12-28 00:20:39 -0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(name: 'Patrick', username: 'Mukula', email: 'patrickkeita80@yahoo.com', password: 'Teruska')
