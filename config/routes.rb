5762b16814c8f83c4cda493e40b56e553caff1e1
unknown <patrickkeita80@yahoo.com>
2024-01-04 08:42:56 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :books
-      resources :users
-      resources :shelves
+      resources :books 
+      resources :shelves  
+      resources :users, only: [] do
+        collection do
+          post 'register', to: 'users#register'
+          get 'login', to: 'users#login'
+          get 'current_user_id', to: 'users#current_user_id'
+        end
+      end
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
