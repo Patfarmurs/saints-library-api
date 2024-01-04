dea5fca3f8d4f2367724338779123eeebbeaaded
unknown <patrickkeita80@yahoo.com>
2024-01-04 08:34:51 -0800

    Update book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
-  belongs_to :author
-  belongs_to :shelf
+  has_many :shelves, dependent: :destroy
+  has_many :users, through: :reservations, dependent: :destroy
 
   validates :title, presence: true, uniqueness: { case_sensitive: false }
+  validates :author, presence: true
+  validates :body, presence: true
 end
