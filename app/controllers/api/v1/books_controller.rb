df32247232ac7d01e15a9f5d65ad3a9c3700457a
unknown <patrickkeita80@yahoo.com>
2024-01-04 08:30:39 -0800

    Update books controller.rb

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,52 +1,36 @@
 class Api::V1::BooksController < ApplicationController
-  before_action :set_book, only: %i[show update destroy]
-
-  # GET /books
   def index
     @books = Book.all
-
     render json: @books
   end
 
-  # GET /books/1
   def show
-    render json: @book
+    book = Book.find(params[:id])
+    render json: book
   end
 
-  # POST /books
   def create
-    @book = Book.new(book_params)
+    book = Book.new(book_params)
 
-    if @book.save
-      render json: @book, status: :created, location: @book
+    if book.save
+      render json: book, status: :created
     else
-      render json: @book.errors, status: :unprocessable_entity
+      render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /books/1
-  def update
-    if @book.update(book_params)
-      render json: @book
-    else
-      render json: @book.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /books/1
   def destroy
-    @book.destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      render json: { message: 'Book was deleted successfully' }
+    else
+      render json: { error: 'Failed to delete the book' }, status: :unprocessable_entity
+    end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_book
-    @book = Book.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :author, :body)
   end
 end
